cbd85e0f223558209e7ca9563b6f20ed2e084d1d
chukak <chukak12@gmail.com>
2018-04-02 22:19:01 +0300

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include "src/videooutput.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    qmlRegisterType<VideoOutput>("backend.VideoOutput", 1, 0, "VideoOutputHandler");
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.addImportPath(QStringLiteral("qrc:/icons"));
+    engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
     return app.exec();
 }
