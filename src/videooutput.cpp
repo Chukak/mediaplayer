808532447f2f0ef4f863155907a64b959979e613
chukak <chukak12@gmail.com>
2018-04-04 22:07:07 +0300

    added properties duration and videoTitle, signals and functions of these properties, added media status signal

--- src/videooutput.cpp
+++ src/videooutput.cpp
@@ -1,10 +1,14 @@
 #include "videooutput.h"
 #include <iostream>
+#include <QtGlobal>
+#include <QMediaMetaData>
 
 VideoOutput::VideoOutput() :
     m_output(0),
     m_player(0),
-    video_url("")
+    video_url(""),
+    video_title(""),
+    video_duration(0)
 {
 
 }
@@ -20,13 +24,49 @@ void VideoOutput::setTargetPlayer(QObject *player)
     QVariant variant = player->property("mediaObject");
     if (variant.canConvert<QMediaPlayer *>()) {
         m_player = variant.value<QMediaPlayer *>();
+        connect(m_player, &QMediaPlayer::metaDataAvailableChanged, this, &VideoOutput::setMetaData);
+        connect(m_player, &QMediaPlayer::mediaStatusChanged, this, &VideoOutput::currentMediaStatus);
     }
     emit targetPlayerChanged();
 }
 
 void VideoOutput::setVideoUrl(const QUrl &url)
 {
-    video_url = url;
-    m_player->setMedia(video_url);
-    emit videoUrlChanged();
+    if (video_url != url) {
+        video_url = url;
+        m_player->setMedia(video_url);
+        emit videoUrlChanged();
+    }
+}
+
+void VideoOutput::setMetaData()
+{
+    setVideoTitle(m_player->metaData(QMediaMetaData::Title).toString());
+}
+
+void VideoOutput::currentMediaStatus(QMediaPlayer::MediaStatus status)
+{
+
+}
+
+void VideoOutput::setVideoTitle(const QString &title)
+{
+    QString new_title;
+    if (!title.isEmpty()) {
+        new_title = title;
+    } else {
+        new_title = video_url.fileName();
+    }
+    if (video_title != new_title) {
+        video_title = new_title;
+        emit videoTitleChanged();
+    }
+}
+
+void VideoOutput::setDuration(qint64 duration)
+{
+    if (video_duration != duration) {
+        video_duration = duration;
+        emit durationChanged();
+    }
 }
--- src/videooutput.h
+++ src/videooutput.h
@@ -11,6 +11,8 @@ class VideoOutput : public QObject
     Q_PROPERTY(QQuickItem *targetOutput READ targetOutput WRITE setTargetOutput NOTIFY targetOutputChanged)
     Q_PROPERTY(QObject *targetPlayer READ targetPlayer WRITE setTargetPlayer NOTIFY targetPlayerChanged)
     Q_PROPERTY(QUrl videoUrl READ videoUrl WRITE setVideoUrl NOTIFY videoUrlChanged)
+    Q_PROPERTY(QString videoTitle READ videoTitle WRITE setVideoTitle NOTIFY videoTitleChanged)
+    Q_PROPERTY(qint64 duration READ duration WRITE setDuration NOTIFY durationChanged)
 
 public:
     explicit VideoOutput();
@@ -24,6 +26,12 @@ public:
     QUrl videoUrl() { return video_url; }
     void setVideoUrl(const QUrl& url);
 
+    QString videoTitle() { return video_title; }
+    void setVideoTitle(const QString& title);
+
+    qint64 duration() { return video_duration; }
+    void setDuration(qint64 duration);
+
 signals:
     void targetOutputChanged();
 
@@ -31,12 +39,21 @@ signals:
 
     void videoUrlChanged();
 
+    void videoTitleChanged();
+
+    void durationChanged();
+
 public slots:
+    void setMetaData();
+
+    void currentMediaStatus(QMediaPlayer::MediaStatus status);
 
 private:
     QQuickItem *m_output;
     QMediaPlayer *m_player;
     QUrl video_url;
+    QString video_title;
+    qint64  video_duration;
 };
 
 #endif // VIDEOOUTPUT_H
