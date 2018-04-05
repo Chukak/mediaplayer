7c6d5faa0c8b965f16a29f6ce120bb24b2d0dde7
chukak <chukak12@gmail.com>
2018-04-05 21:06:53 +0300

    new qml type - backend.MediaPlayer

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include "src/videooutput.h"
+#include "src/mediaplayer.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     qmlRegisterType<VideoOutput>("backend.VideoOutput", 1, 0, "VideoOutputHandler");
+    qmlRegisterType<MediaPlayer>("backend.MediaPlayer", 1, 0, "MediaPlayerHandler");
 
     QQmlApplicationEngine engine;
     engine.addImportPath(QStringLiteral("qrc:/icons"));
