4c98e41955afecde476ece5c85546107133209bb
chukak <chukak12@gmail.com>
2018-04-05 21:06:17 +0300

    removed unused properties, signals, functions, added property to video status

--- src/videooutput.cpp
+++ src/videooutput.cpp
@@ -3,14 +3,9 @@
 #include <QtGlobal>
 #include <QMediaMetaData>
 
-VideoOutput::VideoOutput() :
-    m_output(0),
-    m_player(0),
-    video_url(""),
-    video_title(""),
-    video_duration(0)
+VideoOutput::VideoOutput()
 {
-
+    m_player = new MediaPlayer(this);
 }
 
 void VideoOutput::setTargetOutput(QQuickItem *output)
@@ -19,54 +14,10 @@ void VideoOutput::setTargetOutput(QQuickItem *output)
     emit targetOutputChanged();
 }
 
-void VideoOutput::setTargetPlayer(QObject *player)
+void VideoOutput::setStatus(const QString &status)
 {
-    QVariant variant = player->property("mediaObject");
-    if (variant.canConvert<QMediaPlayer *>()) {
-        m_player = variant.value<QMediaPlayer *>();
-        connect(m_player, &QMediaPlayer::metaDataAvailableChanged, this, &VideoOutput::setMetaData);
-        connect(m_player, &QMediaPlayer::mediaStatusChanged, this, &VideoOutput::currentMediaStatus);
-    }
-    emit targetPlayerChanged();
-}
-
-void VideoOutput::setVideoUrl(const QUrl &url)
-{
-    if (video_url != url) {
-        video_url = url;
-        m_player->setMedia(video_url);
-        emit videoUrlChanged();
-    }
-}
-
-void VideoOutput::setMetaData()
-{
-    setVideoTitle(m_player->metaData(QMediaMetaData::Title).toString());
-}
-
-void VideoOutput::currentMediaStatus(QMediaPlayer::MediaStatus status)
-{
-
-}
-
-void VideoOutput::setVideoTitle(const QString &title)
-{
-    QString new_title;
-    if (!title.isEmpty()) {
-        new_title = title;
-    } else {
-        new_title = video_url.fileName();
-    }
-    if (video_title != new_title) {
-        video_title = new_title;
-        emit videoTitleChanged();
-    }
-}
-
-void VideoOutput::setDuration(qint64 duration)
-{
-    if (video_duration != duration) {
-        video_duration = duration;
-        emit durationChanged();
+    if (m_status != status) {
+        m_status = status;
+        emit statusChanged();
     }
 }
--- src/videooutput.h
+++ src/videooutput.h
@@ -1,18 +1,16 @@
 #ifndef VIDEOOUTPUT_H
 #define VIDEOOUTPUT_H
 
+#include "mediaplayer.h"
 #include <QObject>
 #include <QQuickItem>
-#include <QMediaPlayer>
+
 
 class VideoOutput : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QQuickItem *targetOutput READ targetOutput WRITE setTargetOutput NOTIFY targetOutputChanged)
-    Q_PROPERTY(QObject *targetPlayer READ targetPlayer WRITE setTargetPlayer NOTIFY targetPlayerChanged)
-    Q_PROPERTY(QUrl videoUrl READ videoUrl WRITE setVideoUrl NOTIFY videoUrlChanged)
-    Q_PROPERTY(QString videoTitle READ videoTitle WRITE setVideoTitle NOTIFY videoTitleChanged)
-    Q_PROPERTY(qint64 duration READ duration WRITE setDuration NOTIFY durationChanged)
+    Q_PROPERTY(QString status READ status WRITE setStatus NOTIFY statusChanged)
 
 public:
     explicit VideoOutput();
@@ -20,40 +18,20 @@ public:
     QQuickItem *targetOutput() { return m_output; }
     void setTargetOutput(QQuickItem *output);
 
-    QMediaPlayer *targetPlayer() { return m_player; }
-    void setTargetPlayer(QObject *player);
-
-    QUrl videoUrl() { return video_url; }
-    void setVideoUrl(const QUrl& url);
-
-    QString videoTitle() { return video_title; }
-    void setVideoTitle(const QString& title);
-
-    qint64 duration() { return video_duration; }
-    void setDuration(qint64 duration);
+    QString status() { return m_status; }
+    void setStatus(const QString& status);
 
 signals:
     void targetOutputChanged();
 
-    void targetPlayerChanged();
-
-    void videoUrlChanged();
-
-    void videoTitleChanged();
-
-    void durationChanged();
+    void statusChanged();
 
 public slots:
-    void setMetaData();
-
-    void currentMediaStatus(QMediaPlayer::MediaStatus status);
 
 private:
-    QQuickItem *m_output;
-    QMediaPlayer *m_player;
-    QUrl video_url;
-    QString video_title;
-    qint64  video_duration;
+    QQuickItem *m_output = nullptr;
+    MediaPlayer *m_player = nullptr;
+    QString m_status = "";
 };
 
 #endif // VIDEOOUTPUT_H
