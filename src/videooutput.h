5ddfca8f184bb450760422d452335aa4ea1dbb7e
chukak <chukak12@gmail.com>
2018-04-08 18:04:38 +0300

    added property for media player, functions for status update, updated constructor parameters

--- src/videooutput.h
+++ src/videooutput.h
@@ -10,28 +10,36 @@ class VideoOutput : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QQuickItem *targetOutput READ targetOutput WRITE setTargetOutput NOTIFY targetOutputChanged)
-    Q_PROPERTY(QString status READ status WRITE setStatus NOTIFY statusChanged)
+    Q_PROPERTY(QString status READ status NOTIFY statusChanged)
+    Q_PROPERTY(QObject *mediaPlayer READ mediaPlayer WRITE setMediaPlayer NOTIFY mediaPlayerChanged)
 
 public:
-    explicit VideoOutput();
+    explicit VideoOutput(QObject *parent = 0);
 
     QQuickItem *targetOutput() { return m_output; }
     void setTargetOutput(QQuickItem *output);
 
+    QObject *mediaPlayer() { return m_player; }
+    void setMediaPlayer(QObject *player);
+
     QString status() { return m_status; }
-    void setStatus(const QString& status);
 
 signals:
     void targetOutputChanged();
 
+    void mediaPlayerChanged();
+
     void statusChanged();
 
-public slots:
+private slots:
+    void updateState(const QMediaPlayer::State& state);
+
+    void updateStatus(const QMediaPlayer::MediaStatus& status);
 
 private:
     QQuickItem *m_output = nullptr;
     MediaPlayer *m_player = nullptr;
-    QString m_status = "";
+    QString m_status = "No media";
 };
 
 #endif // VIDEOOUTPUT_H
