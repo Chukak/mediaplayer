5dd900ab2ad6eb6b5f0d25bdf82a484ebe7e37ef
chukak <chukak12@gmail.com>
2018-04-09 19:59:28 +0300

    added signals to the tests, created test for the MediaPlayer object

--- tests/unit-tests/tst_videooutputtest.cpp
+++ tests/unit-tests/tst_videooutputtest.cpp
@@ -2,6 +2,7 @@
 #include <QString>
 #include <QtTest>
 #include <QQuickItem>
+#include <QSignalSpy>
 
 class VideoOutputTest : public QObject
 {
@@ -12,6 +13,11 @@ public:
 
 private Q_SLOTS:
     void testTargetOutput();
+    void testMediaPlayer();
+
+    void cleanup();
+
+private:
 };
 
 VideoOutputTest::VideoOutputTest()
@@ -21,9 +27,35 @@ VideoOutputTest::VideoOutputTest()
 void VideoOutputTest::testTargetOutput()
 {
     VideoOutput output;
+    QSignalSpy spy(&output, &VideoOutput::targetOutputChanged);
+    QCOMPARE(true, spy.isValid());
     QQuickItem *target = new QQuickItem();
     output.setTargetOutput(target);
     QCOMPARE(output.targetOutput(), target);
+    QCOMPARE(1, spy.count());
+    QQuickItem *other_target = new QQuickItem();
+    output.setTargetOutput(other_target);
+    QCOMPARE(output.targetOutput(), other_target);
+    QCOMPARE(2, spy.count());
+}
+
+void VideoOutputTest::testMediaPlayer()
+{
+    VideoOutput output;
+    QSignalSpy spy(&output, &VideoOutput::mediaPlayerChanged);
+    QCOMPARE(true, spy.isValid());
+    MediaPlayer player;
+    output.setMediaPlayer(&player);
+    QCOMPARE(&player, output.mediaPlayer());
+    QCOMPARE(1, spy.count());
+    MediaPlayer other_player;
+    output.setMediaPlayer(&other_player);
+    QCOMPARE(&other_player, output.mediaPlayer());
+    QCOMPARE(2, spy.count());
+}
+
+void VideoOutputTest::cleanup()
+{
 }
 
 QTEST_APPLESS_MAIN(VideoOutputTest)
