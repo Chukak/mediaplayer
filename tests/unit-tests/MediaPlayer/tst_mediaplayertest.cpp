6320e6dd29d491d8b15d1bfb8d2deff248cb394b
chukak <chukak12@gmail.com>
2018-04-10 23:44:16 +0300

    updated cast for QObject and QMediaPlayer, finished testMediaUrl, added test to check the title of the media

--- tests/unit-tests/MediaPlayer/tst_mediaplayertest.cpp
+++ tests/unit-tests/MediaPlayer/tst_mediaplayertest.cpp
@@ -13,6 +13,7 @@ public:
 private Q_SLOTS:
     void testPlayer();
     void testMediaUrl();
+    void testMediaTitle();
 
 private:
 };
@@ -26,14 +27,17 @@ void MediaPlayerTest::testPlayer()
     MediaPlayer media_player;
     QSignalSpy spy(&media_player, &MediaPlayer::playerChanged);
     QCOMPARE(true, spy.isValid());
-    QObject *player = new QObject();
-    QMediaPlayer *m_player = qobject_cast<QMediaPlayer *>(player);
-    media_player.setPlayer(player);
+    QObject player;
+    QMediaPlayer player_value;
+    player.setProperty("mediaObject", QVariant::fromValue(&player_value));
+    QMediaPlayer *m_player = qvariant_cast<QMediaPlayer *>(QVariant::fromValue(player.property("mediaObject")));
+    media_player.setPlayer(&player);
     QCOMPARE(m_player, media_player.player());
     QCOMPARE(1, spy.count());
-    QObject *other_player = new QObject();
-    QMediaPlayer *m_other_player = qobject_cast<QMediaPlayer *>(other_player);
-    media_player.setPlayer(other_player);
+    QObject other_player;
+    other_player.setProperty("mediaObject", QVariant::fromValue(&player_value));
+    QMediaPlayer *m_other_player = qvariant_cast<QMediaPlayer *>(QVariant::fromValue(other_player.property("mediaObject")));
+    media_player.setPlayer(&other_player);
     QCOMPARE(m_other_player, media_player.player());
     QCOMPARE(2, spy.count());
 }
@@ -43,6 +47,35 @@ void MediaPlayerTest::testMediaUrl()
     MediaPlayer media_player;
     QSignalSpy spy(&media_player, &MediaPlayer::mediaUrlChanged);
     QCOMPARE(true, spy.isValid());
+    QObject player;
+    QMediaPlayer player_value;
+    player.setProperty("mediaObject", QVariant::fromValue(&player_value));
+    QMediaPlayer *m_player = qvariant_cast<QMediaPlayer *>(QVariant::fromValue(player.property("mediaObject")));
+    media_player.setPlayer(&player);
+    QCOMPARE(m_player, media_player.player());
+    QUrl media_url("path/to/video");
+    media_player.setMediaUrl(media_url);
+    QCOMPARE(media_player.mediaUrl(), media_url);
+    QCOMPARE(1, spy.count());
+    QUrl other_media_url("path/to/other/video");
+    media_player.setMediaUrl(other_media_url);
+    QCOMPARE(media_player.mediaUrl(), other_media_url);
+    QCOMPARE(2, spy.count());
+}
+
+void MediaPlayerTest::testMediaTitle()
+{
+    MediaPlayer media_player;
+    QSignalSpy spy(&media_player, &MediaPlayer::mediaTitleChanged);
+    QCOMPARE(true, spy.isValid());
+    QString title("valid_title");
+    media_player.setMediaTitle(title);
+    QCOMPARE(media_player.mediaTitle(), title);
+    QCOMPARE(1, spy.count());
+    QString other_title("valid_title2");
+    media_player.setMediaTitle(other_title);
+    QCOMPARE(media_player.mediaTitle(), other_title);
+    QCOMPARE(2, spy.count());
 }
 
 QTEST_APPLESS_MAIN(MediaPlayerTest)
