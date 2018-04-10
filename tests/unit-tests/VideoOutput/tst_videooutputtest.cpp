65fa743c2518d17255ef722470627dc09b73c9a3
chukak <chukak12@gmail.com>
2018-04-10 18:59:14 +0300

    created tests for the targetOutput and mediaPlayer properties

--- tests/unit-tests/VideoOutput/tst_videooutputtest.cpp
+++ tests/unit-tests/VideoOutput/tst_videooutputtest.cpp
@@ -1,5 +1,7 @@
-#include <QString>
+#include "../../../src/videooutput.h"
 #include <QtTest>
+#include <QQuickItem>
+#include <QSignalSpy>
 
 class VideoOutputTest : public QObject
 {
@@ -9,26 +11,50 @@ public:
     VideoOutputTest();
 
 private Q_SLOTS:
-    void initTestCase();
-    void cleanupTestCase();
-    void testCase1();
+    void testTargetOutput();
+    void testMediaPlayer();
+
+    void cleanup();
+
+private:
 };
 
 VideoOutputTest::VideoOutputTest()
 {
 }
 
-void VideoOutputTest::initTestCase()
+void VideoOutputTest::testTargetOutput()
 {
+    VideoOutput output;
+    QSignalSpy spy(&output, &VideoOutput::targetOutputChanged);
+    QCOMPARE(true, spy.isValid());
+    QQuickItem *target = new QQuickItem();
+    output.setTargetOutput(target);
+    QCOMPARE(output.targetOutput(), target);
+    QCOMPARE(1, spy.count());
+    QQuickItem *other_target = new QQuickItem();
+    output.setTargetOutput(other_target);
+    QCOMPARE(output.targetOutput(), other_target);
+    QCOMPARE(2, spy.count());
 }
 
-void VideoOutputTest::cleanupTestCase()
+void VideoOutputTest::testMediaPlayer()
 {
+    VideoOutput output;
+    QSignalSpy spy(&output, &VideoOutput::mediaPlayerChanged);
+    QCOMPARE(true, spy.isValid());
+    MediaPlayer player;
+    output.setMediaPlayer(&player);
+    QCOMPARE(&player, output.mediaPlayer());
+    QCOMPARE(1, spy.count());
+    MediaPlayer other_player;
+    output.setMediaPlayer(&other_player);
+    QCOMPARE(&other_player, output.mediaPlayer());
+    QCOMPARE(2, spy.count());
 }
 
-void VideoOutputTest::testCase1()
+void VideoOutputTest::cleanup()
 {
-    QVERIFY2(true, "Failure");
 }
 
 QTEST_APPLESS_MAIN(VideoOutputTest)
