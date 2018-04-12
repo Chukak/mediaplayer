47470b8954dccd32e36b59d51a9b2d25e9bd7210
chukak <chukak12@gmail.com>
2018-04-12 20:56:31 +0300

    update class subtitle item

--- src/subtitleitem.h
+++ src/subtitleitem.h
@@ -1,4 +1,32 @@
 #ifndef SUBTITLEITEM_H
 #define SUBTITLEITEM_H
 
+#include <QtCore>
+
+class  SubtitleItem {
+public:
+    SubtitleItem(qint32 number, qint64 start, qint64 end, QString string) :
+        number_section(number),
+        time_start(start),
+        time_end(end),
+        _text(string)
+    {
+    }
+
+    qint32 section() { return number_section; }
+
+    qint64 timeStart() { return time_start; }
+
+    qint64 timeEnd() { return time_end; }
+
+    QString text() { return _text; }
+
+private:
+    qint32 number_section;
+    qint64 time_start;
+    qint64 time_end;
+    QString _text;
+};
+
+
 #endif // SUBTITLEITEM_H
