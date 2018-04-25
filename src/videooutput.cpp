a802dfdffbe41deb5592f321b8e6c99305d7eeea
chukak <chukak12@gmail.com>
2018-04-25 23:54:38 +0300

    added function to get a unique file name, added documentation and comments

--- src/videooutput.cpp
+++ src/videooutput.cpp
@@ -2,26 +2,33 @@
 #include <QPixmap>
 #include <QImage>
 
-
+/*
+ * Function for conversion from YUV420P format to RGB32 format.
+ * Convert a video frame to an image.
+ * Return QImage in RGB32 format.
+ * @param data - a pointer toe the start of the frame data duffer.
+ * @param width - width of a video frame.
+ * @param height - height of a video frame.
+ */
 QImage convertYUV420pToRGB32(const uchar *data, int width, int height)
 {
     if (width > 0 && height > 0) {
         QImage image(width, height, QImage::Format_RGB32);
-        for (int y=0; y<height; ++y) {
-            const uchar *yLine = data + y*width;
-            const uchar *uLine = data + width*height + (y/2)*width/2;
-            const uchar *vLine = data + width*height*5/4 + (y/2)*width/2;
+        for (int y = 0; y < height; ++y) {
+            const uchar *yLine = data + y * width;
+            const uchar *uLine = data + width * height + (y / 2) * width / 2;
+            const uchar *vLine = data + width * height * 5 / 4 + (y / 2) * width / 2;
 
-            for (int x=0; x<width; ++x) {
-                const qreal Y = 1.164*(yLine[x]-16);
-                const int U = uLine[x/2]-128;
-                const int V = vLine[x/2]-128;
+            for (int x = 0; x < width; ++x) {
+                const qreal Y = 1.164 * (yLine[x] - 16);
+                const int U = uLine[x / 2] - 128;
+                const int V = vLine[x / 2] - 128;
 
-                int b = qBound(0, int(Y + 2.018*U), 255);
-                int g = qBound(0, int(Y - 0.813*V - 0.391*U), 255);
-                int r = qBound(0, int(Y + 1.596*V), 255);
+                int b = qBound(0, int(Y + 2.018 * U), 255);
+                int g = qBound(0, int(Y - 0.813 * V - 0.391 * U), 255);
+                int r = qBound(0, int(Y + 1.596 * V), 255);
 
-                image.setPixel(x,y,qRgb(r,g,b));
+                image.setPixel(x, y, qRgb(r, g, b));
             }
         }
         return image;
@@ -29,39 +36,65 @@ QImage convertYUV420pToRGB32(const uchar *data, int width, int height)
     return QImage();
 }
 
+/*
+ * Returns the unique name of the file.
+ */
+QString getUniqueName(const QString& path)
+{
+    QTemporaryFile file(path +  QDir::separator() + "screenshot_XXXXXX");
+    if (!file.open()) {
+        return QString("");
+    }
+    file.close();
+    return QString(file.fileName());
+}
+
+
 VideoOutput::VideoOutput(QObject *parent) :
     QObject(parent)
 {
 
 }
 
+/*
+ * Save the `VideoOutput` object from qml.
+ */
 void VideoOutput::setTargetOutput(QQuickItem *output)
 {
     m_output = output;
     emit targetOutputChanged();
 }
 
+/*
+ * Save the `MediaObject` object from qml.
+ */
 void VideoOutput::setMediaPlayer(MediaPlayer *player)
 {
     m_player = player;
     m_player->setParent(this);
-    connect(qobject_cast<QMediaPlayer *>(m_player->player()), &QMediaPlayer::stateChanged,
-            this, &VideoOutput::updateState);
-    connect(qobject_cast<QMediaPlayer *>(m_player->player()), &QMediaPlayer::mediaStatusChanged,
-            this, &VideoOutput::updateStatus);
-    connect(qobject_cast<QMediaPlayer *>(m_player->player()), &QMediaPlayer::playbackRateChanged,
-            this, &VideoOutput::snapshot);
+    // Cast `QObject *` to `QMediaPlayer *`.
+    QMediaPlayer *p_player = qobject_cast<QMediaPlayer *>(m_player->player());
+    connect(p_player, &QMediaPlayer::stateChanged, this, &VideoOutput::updateState);
+    connect(p_player, &QMediaPlayer::mediaStatusChanged, this, &VideoOutput::updateStatus);
+    connect(p_player, &QMediaPlayer::playbackRateChanged, this, &VideoOutput::snapshot);
+    // Object for update the current video frame.
     probe = new QVideoProbe();
-    probe->setSource(qobject_cast<QMediaPlayer *>(m_player->player()));
+    probe->setSource(p_player);
     connect(probe, &QVideoProbe::videoFrameProbed, this, &VideoOutput::updateCurrentFrame);
     emit mediaPlayerChanged();
 }
 
+/*
+ * Save the current video frame
+ */
 void VideoOutput::updateCurrentFrame(const QVideoFrame &frame)
 {
     current_frame = frame;
 }
 
+/*
+ * Changes the state of the media.
+ */
 void VideoOutput::updateState(const QMediaPlayer::State& state)
 {
     switch (state) {
@@ -78,6 +111,9 @@ void VideoOutput::updateState(const QMediaPlayer::State& state)
     emit statusChanged();
 }
 
+/*
+ * Changes the status of the media.
+ */
 void VideoOutput::updateStatus(const QMediaPlayer::MediaStatus& status)
 {
     switch (status) {
@@ -102,28 +138,39 @@ void VideoOutput::updateStatus(const QMediaPlayer::MediaStatus& status)
     emit statusChanged();
 }
 
+/*
+ * Make a screenshot from the video frame.
+ */
 void VideoOutput::snapshot()
 {
+    // Pauses the video.
     qobject_cast<QMediaPlayer *>(m_player->player())->pause();
     QImage image;
     QVideoFrame frame(current_frame);
+    // Map the content of a video frame to system memory.
     if (!frame.map(QAbstractVideoBuffer::ReadOnly)) {
         return ;
     }
+    // Convert the video frame to the image.
     image = convertYUV420pToRGB32(frame.bits(), frame.width(), frame.height());
+    // Release memory.
     frame.unmap();
+    // Continue to play.
     qobject_cast<QMediaPlayer *>(m_player->player())->play();
     if (image.isNull()) {
         return ;
     }
     QPixmap screenshot = QPixmap::fromImage(image);
+    // Creates a directory for saving screenshots.
+    // For example: `/home/$(user)/Pictures/player_screenshots/` .
     QDir directory(QStandardPaths::writableLocation(QStandardPaths::PicturesLocation) +
                    QDir::separator() + "player_screenshots");
     if (!directory.exists()) {
         QDir().mkpath(directory.path());
     }
-    QString filename = "screen1.png";
-    QFile file(directory.path() + QDir::separator() +  filename);
+    // Gets the unique name of the file.
+    QString filename = getUniqueName(directory.path());
+    QFile file(filename + ".png");
     if (!file.open(QIODevice::WriteOnly)) {
         return ;
     }
--- src/videooutput.h
+++ src/videooutput.h
@@ -6,39 +6,108 @@
 #include <QQuickItem>
 #include <QVideoProbe>
 
-
+/*
+ * Class video output.
+ * Used for displaying video output, status, and creating screenshots.
+ * Inherits QObject.
+ */
 class VideoOutput : public QObject
 {
     Q_OBJECT
+    /*
+     * A property that inherits object the `VideoOutput` from qml.
+     * Readable, can be changed, has signal `targetOutputChanged`.
+     */
     Q_PROPERTY(QQuickItem *targetOutput READ targetOutput WRITE setTargetOutput NOTIFY targetOutputChanged)
+    /*
+     * Property for displaying media status.
+     * Read-only, has signal `statusChanged`.
+     */
     Q_PROPERTY(QString status READ status NOTIFY statusChanged)
+    /*
+     * Property for getting the `MediaPlayer` object.
+     * Readable, can be changed, has signal `mediaPlayerChanged`.
+     */
     Q_PROPERTY(MediaPlayer *mediaPlayer READ mediaPlayer WRITE setMediaPlayer NOTIFY mediaPlayerChanged)
 
 public:
+
+    /*
+     * A constructor.
+     */
     explicit VideoOutput(QObject *parent = 0);
 
+    /*
+     * Returns the `VideoOutput` object in qml.
+     */
     QQuickItem *targetOutput() const { return m_output; }
+
+    /*
+     * Gets the `VideoOutput` object from qml and save this object.
+     * Emits `targetOutputChanged` signal.
+     * @param output - object from qml
+     */
     void setTargetOutput(QQuickItem *output);
 
+    /*
+     * Returns the `MediaPlayer` object in qml.
+     */
     MediaPlayer *mediaPlayer() const { return m_player; }
+
+    /*
+     * Gets the `MediaPlayer` object from qml and save this object.
+     * Emits `mediaPlayerChanged` signal.
+     * @param player - the `MediaPlayer` object.
+     */
     void setMediaPlayer(MediaPlayer *player);
 
+    /*
+     * Returns the current status of the media.
+     */
     QString status() const { return m_status; }
 
+    /*
+     * Creates a screenshot from the video frame.
+     * Called from qml.
+     */
     Q_INVOKABLE void snapshot();
 
 signals:
+
+    /*
+     * Emitted when current the `VideoOutput` object changed (from qml).
+     */
     void targetOutputChanged();
 
+    /*
+     * Emitted when the `MediaPlayer` object changed (from qml).
+     */
     void mediaPlayerChanged();
 
+    /*
+     * Emitted the media status changed.
+     */
     void statusChanged();
 
 public slots:
+
+    /*
+     * Save the current video frame.
+     * Called when a video frame is updated.
+     * @param frame - a video frame
+     */
     void updateCurrentFrame(const QVideoFrame& frame);
 
+    /*
+     * Changes the current state of the media player.
+     * @param state - a state of the media player.
+     */
     void updateState(const QMediaPlayer::State& state);
 
+    /*
+     * Changes the current status of the media player.
+     * @param state - a status of the media player.
+     */
     void updateStatus(const QMediaPlayer::MediaStatus& status);
 
 private:
