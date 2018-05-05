e15f3d121a208f7888ada5da219cded7e3086121
chukak17 <chukak12@gmail.com>
2018-05-05 20:13:29 +0300

    Added documentation and comments.

--- src/mediaplayer.cpp
+++ src/mediaplayer.cpp
@@ -1,9 +1,11 @@
 #include "mediaplayer.h"
 #include <QMediaMetaData>
 #include <QTime>
-#include <QTimer>
-#include <iostream>
 
+/*
+ * Returns the information about the duration of the video in the format "hh:mm:ss" or "mm:ss",
+ * "hh" - hours, "mm" - minutes, "ss" - seconds.
+ */
 QString getFormatDuration(const qint64 duration)
 {
     QString format = duration > 3600 ? "hh:mm:ss" : "mm:ss";
@@ -17,12 +19,14 @@ MediaPlayer::MediaPlayer(QObject *parent)
     setParent(parent);
 }
 
-
+/*
+ * Save the `QMediaObject` from qml.
+ */
 void MediaPlayer::setPlayer(const QObject *player)
 {
-    QVariant variant = player->property("mediaObject");
+    QVariant variant = player->property("mediaObject"); // Get a media object.
     if (variant.canConvert<QMediaPlayer *>()) {
-        m_player = variant.value<QMediaPlayer *>();
+        m_player = variant.value<QMediaPlayer *>(); // Convert QObject to QMediaObject.
         connect(m_player, &QMediaPlayer::metaDataAvailableChanged, this, &MediaPlayer::setMetaData);
         connect(m_player, &QMediaPlayer::positionChanged, this, &MediaPlayer::updatePosition);
         connect(m_player, &QMediaPlayer::durationChanged, this, &MediaPlayer::updateDuration);
@@ -30,15 +34,21 @@ void MediaPlayer::setPlayer(const QObject *player)
     }
 }
 
+/*
+ * Save the metadata from the video.
+ */
 void MediaPlayer::setMetaData()
 {
     setMediaTitle(m_player->metaData(QMediaMetaData::Title).toString());
 }
 
+/*
+ * Open a media content from the url.
+ */
 void MediaPlayer::setMediaUrl(const QUrl &url)
 {
     if (media_url != url) {
-        QMediaContent content(url);
+        QMediaContent content(url); // Set content.
         if (!content.isNull()) {
             media_url = url;
             m_player->setMedia(content);
@@ -47,13 +57,16 @@ void MediaPlayer::setMediaUrl(const QUrl &url)
     }
 }
 
+/*
+ * Sets the title of the video.
+ */
 void MediaPlayer::setMediaTitle(const QString &title)
 {
     QString new_title;
     if (!title.isEmpty()) {
-        new_title = title;
+        new_title = title; // An embedded title.
     } else {
-        new_title = media_url.fileName();
+        new_title = media_url.fileName(); // A name of the video.
     }
     if (media_title != new_title) {
         media_title = new_title;
@@ -61,63 +74,84 @@ void MediaPlayer::setMediaTitle(const QString &title)
     }
 }
 
+/*
+ * Updates the duration of the video.
+ */
 void MediaPlayer::updateDuration(qint64 duration)
 {
     if (media_duration != duration) {
         media_duration = duration;
         emit durationChanged();
     }
-    updateTotalDuration();
+    updateTotalDuration(); // Update the information about the duration of the video.
 }
 
+/*
+ * Updates the position of the video.
+ */
 void MediaPlayer::updatePosition(qint64 position)
 {
 
     if (position <= media_duration && position >= 0) {
         m_current_position = position;
-        updateDurationInfo(m_current_position / 1000);
+        updateDurationInfo(m_current_position / 1000); // Update the information about the position.
         emit _positionChanged();
         emit durationInfoChanged();
     }
 }
 
-
+/*
+ * Updates the information about the position of the video.
+ */
 void MediaPlayer::updateDurationInfo(qint64 info)
 {
     if (media_duration) {
+        // Set time from milliseconds.
         QTime current_time((info / 3600) % 60, (info / 60) % 60,
                          (info % 60));
+        // Get the format.
         QString format = getFormatDuration(media_duration / 1000);
         duration_info = current_time.toString(format);
         emit durationInfoChanged();
     }
 }
 
+/*
+ * Updates the duration of the video.
+ */
 void MediaPlayer::updateTotalDuration()
 {
     if (media_duration) {
         qint64 duration = media_duration / 1000;
-
+        // Set tile from milliseconds.
         QTime total_time((duration / 3600) % 60, (duration / 60) % 60,
                          (duration % 60));
+        // Get the format.
         QString format = getFormatDuration(duration);
         total_duration = total_time.toString(format);
         emit totalDurationChanged();
     }
 }
 
+/*
+ * Rewinding the video.
+ */
 void MediaPlayer::seek(qint64 position)
 {
     qint64 pos = position > 0 ? position * 1000 : 0;
     if (pos > 0 && pos > media_duration){
         pos = media_duration;
     }
+    // Check the subtitles.
     if (subtitles_output && subtitles_output->selected()) {
         subtitles_output->updateSubtitlesText(pos, true);
     }
     m_player->setPosition(pos);
 }
 
+/*
+ * Sets the `SubtitlesOutput` object.
+ */
 void MediaPlayer::setSubtitlesOutput(SubtitlesOutput *subs_out)
 {
     subtitles_output = subs_out;
@@ -130,14 +164,20 @@ void MediaPlayer::setSubtitlesOutput(SubtitlesOutput *subs_out)
     emit subtitlesOutputChanged();
 }
 
+/*
+ * Displaying the subtitles.
+ */
 void MediaPlayer::showSubtitles(bool show)
 {
     if (subtitles_output->selected()) {
         subtitles_output->setSelected(show);
-        subtitles_output->empty();
+        subtitles_output->empty(); // Clear.
     }
 }
 
+/*
+ * Sets the speed of video playback.
+ */
 void MediaPlayer::setPlaybackRate(double rate)
 {
     if (rate >= -10.0 && rate <= 10.0 &&
@@ -146,4 +186,3 @@ void MediaPlayer::setPlaybackRate(double rate)
         m_player->setPlaybackRate(rate);
     }
 }
-
--- src/mediaplayer.h
+++ src/mediaplayer.h
@@ -5,73 +5,231 @@
 #include <QMediaPlayer>
 #include "subtitlesoutput.h"
 
+/*
+ * Class for displaying media player. Displays the information about the duration of the video,
+ * the title of video, sets the subtitles to the current video.
+ * Inherits QObject.
+ */
 class MediaPlayer : public QObject
 {
     Q_OBJECT
+    /*
+     * Sets mediaplayer from qml.
+     * Readable, can be changed, has signal `playerChanged`.
+     */
     Q_PROPERTY(QObject *player READ player WRITE setPlayer NOTIFY playerChanged)
+    /*
+     * Sets the media url to qml.
+     * Readable, can be changed, has signal `mediaUrlChanged`.
+     */
     Q_PROPERTY(QUrl mediaUrl READ mediaUrl WRITE setMediaUrl NOTIFY mediaUrlChanged)
+    /*
+     * Sets the media title to qml.
+     * Readable, can be changed, has signal `mediaTitleChanged`.
+     */
     Q_PROPERTY(QString mediaTitle READ mediaTitle WRITE setMediaTitle NOTIFY mediaTitleChanged)
+    /*
+     * A property for displaying the duration of the video.
+     * Read-only, has signal `durationChanged`.
+     */
     Q_PROPERTY(qint64 duration READ duration NOTIFY durationChanged)
+    /*
+     * A property for displaying the current position of the video.
+     * Read-only, has signal `_positionChanged`.
+     */
     Q_PROPERTY(qint64 position READ position NOTIFY _positionChanged)
+    /*
+     * Displays the current position of the video in qml.
+     * Read-only, has signal `durationInfoChanged`.
+     */
     Q_PROPERTY(QString durationInfo READ durationInfo NOTIFY durationInfoChanged)
+    /*
+     * Displays the durationn of the video in qml.
+     * Read-only, has signal `totalDurationChanged`.
+     */
     Q_PROPERTY(QString totalDuration READ totalDuration NOTIFY totalDurationChanged)
 
+    /*
+     * Sets a `SubtitlesOutput` object from qml.
+     * Readable, can be changed, has signal `subtitlesOutputChanged`.
+     */
     Q_PROPERTY(SubtitlesOutput *subtitlesOutput READ subtitlesOutput WRITE setSubtitlesOutput NOTIFY subtitlesOutputChanged)
 
 public:
+
+    /*
+     * A construstor.
+     */
     explicit MediaPlayer(QObject *parent = 0);
 
+    /*
+     * Returns `QMediaObject` as the `QObject` type in qml.
+     */
     QObject *player() const { return m_player; }
+
+    /*
+     * Gets the `QMediaObject` object as the `QObject` type from qml.
+     * Emits `playerChanged` signal.
+     * @param player - a pointer to the `QObject` object.
+     */
     void setPlayer(const QObject *player);
 
+    /*
+     * Returns the media url in qml.
+     */
     QUrl mediaUrl() const { return media_url; }
+
+    /*
+     * Gets the url from qml.
+     * Emits `mediaUrlChanged` signal.
+     * @param url - the video url.
+     */
     void setMediaUrl(const QUrl& url);
 
+    /*
+     * Returns the titles of the media.
+     */
     QString mediaTitle() const { return media_title; }
+
+    /*
+     * Gets the media title from qml.
+     * Emits `mediaTitleChanged` signal.
+     * @param title - the video title.
+     */
     void setMediaTitle(const QString& title);
 
+    /*
+     * Returns the total duration of the video in seconds.
+     */
     qint64 duration() const { return media_duration / 1000; }
 
+    /*
+     * Returns the current position of the video in seconds.
+     */
     qint64 position() const { return m_current_position / 1000; }
 
+    /*
+     * Returns the information about the current position of the video in milliseconds.
+     */
     QString durationInfo() const{ return duration_info; }
 
+    /*
+     * Returns the information about the total duration of the video in milliseconds.
+     */
     QString totalDuration() const { return total_duration; }
 
+    /*
+     * Returns the `SubtitlesOutput` object.
+     */
     SubtitlesOutput *subtitlesOutput() const { return subtitles_output; }
+
+    /*
+     * Gets the `Subtitlesoutput` object.
+     * Emits `subtitlesOutputChanged` signal.
+     * @param subs_out - a pointer to the `SubtitlesOutput` object.
+     */
     void setSubtitlesOutput(SubtitlesOutput *subs_out);
 
+    /*
+     * Displays the subtitles.
+     * Called from qml.
+     * @param show - `true` or `false`.
+     */
     Q_INVOKABLE void showSubtitles(bool show);
 
+    /*
+     * Sets the speed of video playback.
+     * Called from qml.
+     * @param rate - speed.
+     */
     Q_INVOKABLE void setPlaybackRate(double rate);
 
 signals:
+
+    /*
+     * Emitted when current the `QMediaObject` object changed (from qml).
+     */
     void playerChanged();
 
+    /*
+     * Emitted when the url changed (from qml).
+     */
     void mediaUrlChanged();
 
+    /*
+     * Emitted when the current title changed (from qml).
+     */
     void mediaTitleChanged();
 
+    /*
+     * Emitted when the total duration changed.
+     */
     void durationChanged();
 
+    /*
+     * Emitted when the information of the current position changed.
+     */
     void durationInfoChanged();
 
+    /*
+     * Emitted when the current position changed.
+     */
     void _positionChanged();
 
+    /*
+     * Emitted when the information of the total duration changed.
+     */
     void totalDurationChanged();
 
+    /*
+     * Emitted when current the `SubtitlesOutput` object changed (from qml).
+     */
     void subtitlesOutputChanged();
 
 public slots:
+
+    /*
+     * Sets the current postion of the video. Rewind the video.
+     * Called from qml.
+     * @param position - position in seconds.
+     */
     void seek(qint64 position);
 
 private slots:
+
+    /*
+     * Sets the metadata from the video.
+     * Called when the video is available.
+     */
     void setMetaData();
 
 private:
+
+    /*
+     * Updates the current position of the video.
+     * Called the position of the video changed.
+     * @param position - a new position.
+     */
     void updatePosition(qint64 position);
+
+    /*
+     * Updates the information about the current position of the video.
+     * Called the position of the video updated.
+     * @param info - the position of the video.
+     */
     void updateDurationInfo(qint64 info);
+
+    /*
+     * Updates the information about the total duration of the video.
+     * Called the duration of the video updated.
+     */
     void updateTotalDuration();
+
+    /*
+     * Updates the duration of the video.
+     * Called the duration of the video changed.
+     * @param duration - the duration of the video.
+     */
     void updateDuration(qint64 duration);
 
     QMediaPlayer *m_player = nullptr;
