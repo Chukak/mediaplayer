34e18b1ac94503b43610d467da6b46fd3cc65c65
chukak17 <chukak12@gmail.com>
2018-05-05 18:22:51 +0300

    removed pause/play player from snapshot function, updated convertYUV420pToRGB32 function

--- src/videooutput.cpp
+++ src/videooutput.cpp
@@ -13,20 +13,21 @@
 QImage convertYUV420pToRGB32(const uchar *data, int width, int height)
 {
     if (width > 0 && height > 0) {
-        QImage image(width, height, QImage::Format_RGB32);
+        //QImage image(width, height, QImage::Format_RGB32);
+        QImage image(width, height, QImage::Format_RGB888);
         for (int y = 0; y < height; ++y) {
             const uchar *yLine = data + y * width;
             const uchar *uLine = data + width * height + (y / 2) * width / 2;
             const uchar *vLine = data + width * height * 5 / 4 + (y / 2) * width / 2;
 
             for (int x = 0; x < width; ++x) {
-                const qreal Y = 1.164 * (yLine[x] - 16);
+                const int Y = 1.164 * (yLine[x] - 16);
                 const int U = uLine[x / 2] - 128;
                 const int V = vLine[x / 2] - 128;
 
-                int b = qBound(0, int(Y + 2.018 * U), 255);
-                int g = qBound(0, int(Y - 0.813 * V - 0.391 * U), 255);
-                int r = qBound(0, int(Y + 1.596 * V), 255);
+                const int b = qBound(0, int(Y + 2.018 * U), 255);
+                const int g = qBound(0, int(Y - 0.813 * V - 0.391 * U), 255);
+                const int r = qBound(0, int(Y + 1.596 * V), 255);
 
                 image.setPixel(x, y, qRgb(r, g, b));
             }
@@ -142,8 +143,6 @@ void VideoOutput::updateStatus(const QMediaPlayer::MediaStatus& status)
  */
 void VideoOutput::snapshot()
 {
-    // Pauses the video.
-    qobject_cast<QMediaPlayer *>(m_player->player())->pause();
     QImage image;
     QVideoFrame frame(current_frame);
     // Map the content of a video frame to system memory.
@@ -155,8 +154,6 @@ void VideoOutput::snapshot()
     image = convertYUV420pToRGB32(frame.bits(), frame.width(), frame.height());
     // Release memory.
     frame.unmap();
-    // Continue to play.
-    qobject_cast<QMediaPlayer *>(m_player->player())->play();
     if (image.isNull()) {
         emit snapshotError("The image is empty.");
         return ;
